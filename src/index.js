8bbd36d944e8330df056c05fd55e2c135cb8bd23
suna <96680507+monstera3@users.noreply.github.com>
2022-03-28 18:05:56 +0900

    未完了リスト追加実装

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // ulタグ生成
+  const ul = document.createElement("ul");
+  ul.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // ulタグの子要素に各要素を設定
+  ul.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("inconplete-list").appendChild(ul);
 };
 
 document
